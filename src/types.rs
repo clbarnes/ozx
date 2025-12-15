ffa1bdc82ad0344d25d0fc437579f22aa8913b2f
Chris Barnes <chris.barnes@gerbi-gmb.de>
2025-12-15 11:46:43 +0000

    Restructure ZIP comment metadata

--- src/types.rs
+++ src/types.rs
@@ -1,6 +1,10 @@
 use serde::{Deserialize, Serialize};
 
-/// {"ome": { "version": "XX.YY" }}
+/// e.g.
+///
+/// ```json
+/// {"ome": { "version": "XX.YY", "centralDirectory": { "jsonFirst": true } }}`
+/// ```
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct ZipComment {
     pub ome: ZipOmeMetadata,
@@ -11,17 +15,51 @@ impl ZipComment {
         Self {
             ome: ZipOmeMetadata {
                 version: version.into(),
-                json_first,
+                zip_file: Some(OmeZipFile::new(json_first)),
             },
         }
     }
+
+    pub fn json_first(&self) -> bool {
+        self.ome
+            .zip_file
+            .as_ref()
+            .and_then(|z| z.central_directory.as_ref())
+            .and_then(|c| c.json_first)
+            .unwrap_or(false)
+    }
+}
+
+#[derive(Debug, Default, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct OmeZipFile {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    central_directory: Option<CentralDirectory>,
+}
+
+impl OmeZipFile {
+    pub fn new(json_first: bool) -> Self {
+        Self {
+            central_directory: Some(CentralDirectory {
+                json_first: Some(json_first),
+            }),
+        }
+    }
+}
+
+#[derive(Debug, Default, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct CentralDirectory {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    json_first: Option<bool>,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct ZipOmeMetadata {
     pub version: String,
-    pub json_first: bool,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub zip_file: Option<OmeZipFile>,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
